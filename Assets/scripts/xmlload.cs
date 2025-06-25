b84b9209936dee608c56cf16fa3cac01d8d3e1c6
倪好成功后 <2415430981@qq.com>
2025-06-25 13:22:15 +0800

    note

--- Assets/scripts/xmlload.cs
+++ Assets/scripts/xmlload.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Xml.Serialization;
-using System.Xml;//使用xm的文件序列化功能
+using System.Xml;
+using UnityEngine.UI;//使用xm的文件序列化功能
+using System.IO; //使用文件输入输出功能
+
 public class hero
 {
     //定义一个结构体hero
@@ -33,8 +36,11 @@ public struct item
 }
 public class xmlload : MonoBehaviour
 {
+    string xmlpath = "D://code/heros.xml"; //xml文件路径
     // Start is called before the first frame update
     List<hero> heros= new List<hero>(); //创建一个英雄列表  
+    public Text xmltext;//数据显示组件
+    string url;
     void Start()
     { // 
         var hero1 = new hero("hero1", 100, 50, 20); // 创建一个英雄
@@ -44,6 +50,23 @@ public class xmlload : MonoBehaviour
         hero2.items.Add(new item("fangju", 3)); // 添加不同物品
         heros.Add(hero1);
         heros.Add(hero2);
+        Debug.Log("位置" + Application.dataPath);//获取asset文件的路径
+        url = Application.dataPath + "/xml/";
+        if(!Directory.Exists(url)) //文件夹是否存在
+        {
+            Directory.CreateDirectory(url); //；里面是文件名称
+        }
+        else
+        {
+            if (File.Exists(url + "file.txt")){
+                var a = File.ReadAllText(url + "file.txt"); //读取文件内容
+                Debug.Log("文件内容：" + a); //输出文件内容
+                File.Delete(url + "file.txt"); //删除文件
+
+                //File.Create()创建文件
+                //File.WriteAllText(文件，内容)写入文件内容
+            }
+        }
     }
 
     // Update is called once per frame
@@ -52,8 +75,12 @@ public class xmlload : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))//将数据保存到xml文件中
         {
             Debug.Log("保存数据到xml文件中"); //输出日志
-            writerxml("heros.xml"); //调用writerxml方法，传入文件名
-        }   
+            writerxml("heros.xml"); 
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            readxmlfile(xmlpath); 
+        }
     }
     void writerxml(string filename)//文件名
     {
@@ -84,10 +111,32 @@ public class xmlload : MonoBehaviour
                 var item = doc.CreateElement("item"); //创建一个item标签
                 bag.AppendChild(item); //将item标签添加到bag标签中
                 item.SetAttribute("name", itmes.name);
-                item.SetAttribute("id", itmes.id.ToString());//XML 元素添加或修改一个属性，属性名是 "name"，属性值是 itme.name
+                item.SetAttribute("id", itmes.id.ToString());
 
             }
         }
         doc.Save("D://code/" + filename); //保存xml文件
+    }
+    void readxmlfile(string filename) 
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(filename); //加载xml文件
+
+        var root = doc.SelectSingleNode("root") as XmlElement; //获取根节点 
+        foreach(XmlElement hero in  root.ChildNodes)
+        {
+            //填写数据到界面里面
+            xmltext.text += hero.GetAttribute("name") + ":{";
+            var bag = hero.SelectSingleNode("bag") as XmlElement; //获取bag节点
+            foreach(XmlElement item in bag.ChildNodes)
+            {
+                xmltext.text += item.GetAttribute("name") + " ";
+
+                xmltext.text += "}\n";
+
+            }
+            
+        }
+
     }
 }
